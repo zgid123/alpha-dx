724c392da88b7f7ba8354d8383be104345ff4353
Dương Tấn Huỳnh Phong <alphanolucifer@gmail.com>
2026-01-24 14:47:46 +0700

    feat(hono): create arktype validator

--- packages/hono/src/core/validators.ts
+++ packages/hono/src/core/validators.ts
@@ -1,19 +1,10 @@
+import { sValidator as sv } from '@hono/standard-validator';
 import { zValidator as zv } from '@hono/zod-validator';
+import type { Type as ArkType } from 'arktype';
 import type { Env, ValidationTargets } from 'hono';
 import type { ZodType } from 'zod';
 
 import { HonoCommonError } from './HonoError';
-
-interface IError {
-  code: string;
-  path: string[];
-  message: string;
-}
-
-interface IDetail {
-  type: string;
-  message: string;
-}
 
 export const zValidator = <
   T extends ZodType,
@@ -27,16 +18,27 @@ export const zValidator = <
   return zv(target, schema, (result) => {
     if (!result.success) {
       const { message } = result.error;
-      const detail = (JSON.parse(message) as IError[]).reduce<
-        Record<string, IDetail>
-      >((result, { code, path, message }) => {
-        result[path.join('.')] = {
-          message,
-          type: code,
-        };
+      const detail = JSON.parse(message);
 
-        return result;
-      }, {});
+      throw HonoCommonError.invalidParams({
+        detail,
+      });
+    }
+  });
+};
+
+export const arkValidator = <
+  T extends ArkType,
+  Target extends keyof ValidationTargets,
+  E extends Env,
+  P extends string,
+>(
+  target: Target,
+  schema: T,
+): ReturnType<typeof sv<T, Target, E, P>> => {
+  return sv(target, schema, (result) => {
+    if (!result.success) {
+      const detail = result.error;
 
       throw HonoCommonError.invalidParams({
         detail,
