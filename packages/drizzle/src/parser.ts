df5dee6cfb972e0d69035d0c044bd61c449e83c2
Dương Tấn Huỳnh Phong <alphanolucifer@gmail.com>
2026-01-26 02:57:58 +0700

    feat(drizzle): add not null to parser

--- packages/drizzle/src/parser.ts
+++ packages/drizzle/src/parser.ts
@@ -3,22 +3,28 @@ import type { DrizzleQueryError } from 'drizzle-orm';
 export interface IDrizzleError {
   detail: string;
   attribute: string;
-  type: 'DUPLICATE' | 'UNKNOWN';
+  type: 'DUPLICATE' | 'NOT_NULL' | 'UNKNOWN';
 }
 
 interface IErrorCause {
   code: string;
   detail: string;
+  column: string;
+  routine: string;
 }
 
 export function parseError(error: DrizzleQueryError): IDrizzleError {
-  const { detail, code } = error.cause as unknown as IErrorCause;
+  const { detail, code, routine, column } =
+    error.cause as unknown as IErrorCause;
   let type: IDrizzleError['type'] = 'UNKNOWN';
   let attribute = '';
 
   if (code === '23505') {
     type = 'DUPLICATE';
     attribute = (detail.split('=')[0] || '').replace(/key|\(|\)/gi, '').trim();
+  } else if (routine === 'ReportNotNullViolationError') {
+    type = 'NOT_NULL';
+    attribute = column;
   }
 
   return {
