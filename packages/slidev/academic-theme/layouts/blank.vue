97a5dba2cb0014540244db949f2419bc700df432
Dương Tấn Huỳnh Phong <alphanolucifer@gmail.com>
2026-09-22 16:14:44 +0700

    feat(slidev-academic-theme): update style for new components

---
+++ packages/slidev/academic-theme/layouts/blank.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { handleBackground } from '@slidev/client';
+import { computed } from 'vue';
+
+const { background } = defineProps({
+  background: {
+    default: '',
+  },
+});
+
+const style = computed(() => handleBackground(background));
+</script>
+
+<template>
+  <main
+    class="slidev-layout alpha-academic-blank"
+    :style="style"
+  >
+    <slot />
+  </main>
+</template>
